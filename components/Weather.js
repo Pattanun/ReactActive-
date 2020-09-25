d7e2ec05c08fcdd119258a6b121c102ccdb4f458
Pattanun <oatinfinity42@gmail.com>
2020-09-25 11:48:36 +0700

    commit1

--- components/Weather.js
+++ components/Weather.js
@@ -52,13 +52,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%'
     },
-    cover: {
-        backgroundColor: 'black',
-        width: '100%',
-        height: 250,
-        opacity: 0.4,
-        alignItems: 'center',
-    },
+    
     medium: {
         marginTop: 32,
         fontSize: 15,
